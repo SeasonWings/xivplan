b42f5132f5bc98c1340d14b6506d6276b42281bd
Maple <1391821287@qq.com>
2025-11-26 23:04:07 +0800

    add 伏凶夔牛岛-夔牛场地图

--- src/presets/nightmare/fxknd.ts
+++ src/presets/nightmare/fxknd.ts
@@ -19,5 +19,14 @@ const BATTLE_3: ArenaPreset = {
     grid: { type: GridType.None },
     backgroundImage: '/arena/zxsj/nightmare/fxknd/fxknd_b3.png',
 };
+const BATTLE_4: ArenaPreset = {
+    name: '伏凶夔牛岛-夔牛-无边框',
+    shape: ArenaShape.Rectangle,
+    width: 600,
+    height: 600,
+    padding: 30,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/zxsj/nightmare/fxknd/fxknd_b4.png',
+};
 
-export const ARENA_PRESETS_NIGHTMARE_FXKND = [BATTLE_1, BATTLE_3];
+export const ARENA_PRESETS_NIGHTMARE_FXKND = [BATTLE_1, BATTLE_3, BATTLE_4];
