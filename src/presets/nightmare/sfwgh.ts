feace35f91be1d4d19388073146469e128a3fc3d
maple <1391821287@qq.com>
2026-01-29 13:51:40 +0800

    add 霜封无垢海B1支持

--- src/presets/nightmare/sfwgh.ts
+++ src/presets/nightmare/sfwgh.ts
@@ -1,5 +1,15 @@
 import { ArenaPreset, ArenaShape, GridType } from '../../scene';
 
+const BATTLE_1: ArenaPreset = {
+    name: '霜封无垢海-慈悲戒师',
+    shape: ArenaShape.Rectangle,
+    width: 600,
+    height: 600,
+    padding: 30,
+    grid: { type: GridType.None },
+    backgroundImage: '/arena/zxsj/nightmare/sfwgh/sfwgh_b1.png',
+};
+
 const BATTLE_2: ArenaPreset = {
     name: '霜封无垢海-慈心霜魂',
     shape: ArenaShape.Rectangle,
@@ -10,4 +20,4 @@ const BATTLE_2: ArenaPreset = {
     backgroundImage: '/arena/zxsj/nightmare/sfwgh/sfwgh_b2.png',
 };
 
-export const ARENA_PRESETS_NIGHTMARE_SFWGH = [BATTLE_2];
+export const ARENA_PRESETS_NIGHTMARE_SFWGH = [BATTLE_1, BATTLE_2];
